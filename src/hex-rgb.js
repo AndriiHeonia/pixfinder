0cec3db2d2670b8b10f0cf30c15dcb5c44d40517
AndreyGeonya <a.geonya@gmail.com>
2013-07-06 20:59:08 +0300

    dramatically slow, but seems working

--- src/hex-rgb.js
+++ src/hex-rgb.js
@@ -60,4 +60,4 @@
         return ((blue | green << 8 | red << 16) | 1 << 24).toString(16).slice(1);
     };
 
-})(this);
+})(PF);
--- src/pixfinder.js
+++ src/pixfinder.js
@@ -1,10 +1,11 @@
-var step = 1;
-var PF = {
+var step = 3;
+var PF = {    
+
     // @todo refactor this ugly function to FP style, add step as argument
     // @todo try to detect similar colors by main color
-    getFeatures: function (img, colors) { // (HTMLImageElement, Object) -> Object
+    getFeatures: function (img, colors) { // (HTMLImageElement, Array) -> Object
         var canv = this._wrapByCanvas(img),
-            colors = this._colorsToRgb(colors),
+            colors = this._extendColors(this._colorsToRgb(colors), 10),
             features = [];
 
         for (var x = 0; x < img.clientWidth; x = x+step) {
@@ -24,7 +25,7 @@ var PF = {
                 nPxs = this._getNeighborPixels(px, {
                     w: canv.width - step, 
                     h: canv.height - step
-                });
+                }, 3);
                 nPxCols = this._getPixelsColors(canv, nPxs);
 
                 // skip if px is not a boundary pixel of the feature
@@ -65,11 +66,31 @@ var PF = {
         return canv;
     },
 
-    _colorsToRgb: function (cols) { // (Object) -> Object
+    _colorsToRgb: function (cols) { // (Array) -> Array
         for (var i = 0; i < cols.length; i++) {
-            cols[i] = toRGB(cols[i]);
+            cols[i] = PF.toRGB(cols[i]);
         };
         return cols;
+    },
+
+    _extendColors: function (cols, step) { // (Array) -> Array
+        var res = [];
+        for (var i = 0; i < cols.length; i++) {
+            res.push(cols[i]);
+            for (var j = 0; j < step; j++) {
+                res.push([cols[i][0] + j, cols[i][1], cols[i][2]]);
+                res.push([cols[i][0] + j, cols[i][1] + j, cols[i][2]]);
+                res.push([cols[i][0] + j, cols[i][1], cols[i][2] + j]);
+                res.push([cols[i][0] + j, cols[i][1] + j, cols[i][2] + j]);
+
+                res.push([cols[i][0], cols[i][1] + j, cols[i][2]]);
+                res.push([cols[i][0], cols[i][1] + j, cols[i][2] + j]);
+
+                res.push([cols[i][0], cols[i][1], cols[i][2] + j]);
+                res.push([cols[i][0] + j, cols[i][1], cols[i][2] + j]);
+            };
+        };
+        return res;
     },
 
     _getPixelColor: function (canvas, px) { // (HTMLCanvasElement, Object) -> Array
@@ -121,41 +142,122 @@ var PF = {
         return false;
     },
 
-    _getNeighborPixels: function (px, imgSize) { // (Object, Object) -> Array
+    _getNeighborPixels: function (px, imgSize, count) { // (Object, Object) -> Array
         var res = [];
-        
+
         if (px.x > 0 && px.y > 0) {
-            res.push({ x: px.x-step, y: px.y-step }); // tl
+            res.push(this._getNeighborTlPixels(px, imgSize)); // tl
         };
 
         if (px.y > 0) {
-            res.push({ x: px.x,   y: px.y-step }); // t
+            res.push(this._getNeighborTPixels(px, imgSize)); // t
         };
 
         if (px.x < imgSize.w && px.y > 0) {
-            res.push({ x: px.x+step, y: px.y-step }); // tr
+            res.push(this._getNeighborTrPixels(px, imgSize)); // tr
         };
 
         if (px.x < imgSize.w) {
-            res.push({ x: px.x+step, y: px.y }); // r
+            res.push(this._getNeighborRPixels(px, imgSize)); // r
         };
 
         if (px.x < imgSize.w && px.y < imgSize.h) {
-            res.push({ x: px.x+step, y: px.y+step }); // br
+            res.push(this._getNeighborBrPixels(px, imgSize)); // br
         };
 
         if (px.y < imgSize.h) {
-            res.push({ x: px.x, y: px.y+step }); // b
+            res.push(this._getNeighborBPixels(px, imgSize)); // b
         };
 
         if (px.x > 0 && px.y < imgSize.h) {
-            res.push({ x: px.x-step, y: px.y+step }); // bl
+            res.push(this._getNeighborBlPixels(px, imgSize)); // bl
         };
 
         if (px.x > 0) {
-            res.push({ x: px.x-step, y: px.y }); // l
+            res.push(this._getNeighborLPixels(px, imgSize)); // l
+        };
+
+        for (var c = 0; c < count; c++) {
+            var nRes = [];
+
+            for (var i = 0; i < res.length; i++) {
+
+                if (res[i].x > 0 && res[i].y > 0) {
+                    nRes.push(this._getNeighborTlPixels(res[i], imgSize));
+                };
+
+                if (res[i].y > 0) {
+                    nRes.push(this._getNeighborTPixels(res[i], imgSize)); // t
+                };
+
+                if (res[i].x < imgSize.w && res[i].y > 0) {
+                    nRes.push(this._getNeighborTrPixels(res[i], imgSize)); // tr
+                };
+
+                if (res[i].x < imgSize.w) {
+                    nRes.push(this._getNeighborRPixels(res[i], imgSize)); // r
+                };
+
+                if (res[i].x < imgSize.w && res[i].y < imgSize.h) {
+                    nRes.push(this._getNeighborBrPixels(res[i], imgSize)); // br
+                };
+
+                if (res[i].y < imgSize.h) {
+                    nRes.push(this._getNeighborBPixels(res[i], imgSize)); // b
+                };
+
+                if (res[i].x > 0 && res[i].y < imgSize.h) {
+                    nRes.push(this._getNeighborBlPixels(res[i], imgSize)); // bl
+                };
+
+                if (res[i].x > 0) {
+                    nRes.push(this._getNeighborLPixels(res[i], imgSize)); // l
+                };
+            };
+
+            for (var i = 0; i < nRes.length; i++) {
+                res.push(nRes[i]);
+            };            
         };
 
         return res;
+    },
+
+    _getNeighborTlPixels: function (px, imgSize) { // (Object, Object) -> Object
+        return { x: px.x-step, y: px.y-step }; // tl
+    },
+
+    _getNeighborTPixels: function (px, imgSize) { // (Object, Object) -> Object
+        return { x: px.x,   y: px.y-step }; // t
+    },
+
+    _getNeighborTrPixels: function (px, imgSize) { // (Object, Object) -> Object
+        return { x: px.x+step, y: px.y-step }; // tr
+    },
+
+    _getNeighborRPixels: function (px, imgSize) { // (Object, Object) -> Object
+        return { x: px.x+step, y: px.y }; // r
+    },
+
+    _getNeighborBrPixels: function (px, imgSize) { // (Object, Object) -> Object
+        return { x: px.x+step, y: px.y+step }; // br
+    },
+
+    _getNeighborBPixels: function (px, imgSize) { // (Object, Object) -> Object
+        if (px.y < imgSize.h) {
+            return { x: px.x, y: px.y+step }; // b
+        };
+    },
+
+    _getNeighborBlPixels: function (px, imgSize) { // (Object, Object) -> Object
+        if (px.x > 0 && px.y < imgSize.h) {
+            return { x: px.x-step, y: px.y+step }; // bl
+        };
+    },
+
+    _getNeighborLPixels: function (px, imgSize) { // (Object, Object) -> Object
+        if (px.x > 0) {
+            return { x: px.x-step, y: px.y }; // l
+        };
     }
 }
