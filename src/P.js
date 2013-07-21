1337c797339bda449e5f2af90a54b4b9202b5179
AndreyGeonya <a.geonya@gmail.com>
2013-07-21 16:25:58 +0300

    accuracy added, step deleted, timing added

--- src/P.js
+++ src/P.js
@@ -1,32 +1,44 @@
-var step = 3;
 var P = {
 
-    getRegionsByColors: function (img, colors) { // (HTMLImageElement, Array) -> Object
-        var canvas = this._wrapByCanvas(img),
+    getRegionsByColors: function (img, colors, options) { // (HTMLImageElement, Array, Object) -> Object
+        var opt = options || {},
+            canvas = this._wrapByCanvas(img),
             colors = this._colorsToRgb(colors),
-            regionsPxs = this._getRegionsPixels(canvas, colors),
-            regions = this._splitRegionsByDist(regionsPxs, 10);
+            regionsPxs, regions;
+
+        opt.accuracy = opt.accuracy || 3;
+
+        var t0 = new Date();
+        regionsPxs = this._getRegionsPixels(canvas, colors, opt.accuracy);
+        var t1 = new Date();
+        console.log('_getRegionsPixels: ', t1 - t0);
+
+        var t0 = new Date();
+        regions = this._splitByDist(regionsPxs, 10);
+        var t1 = new Date();
+        console.log('_splitByDist: ', t1 - t0);
 
         return regions;
     },
 
-    _getRegionsPixels: function (canvas, colors) { // (HTMLCanvasElement, Array) -> Array
-        var res = [];
+    _getRegionsPixels: function (canvas, colors, accuracy) { // (HTMLCanvasElement, Array, Number) -> Array
+        var res = [],
+            ctx = canvas.getContext('2d');
 
-        for (var x = 0; x < img.clientWidth; x = x+step) {
-            for (var y = 0; y < img.clientHeight; y = y+step) {
+        for (var x = 0; x < img.clientWidth; x = x+accuracy) {
+            for (var y = 0; y < img.clientHeight; y = y+accuracy) {
                 var px = { x: x, y: y },
-                    pxCol = this._getPixelColor(canvas, px),
+                    pxCol = this._getPixelColor(ctx, px),
                     nPxs = this._getNeighborPixels(px, {
-                        w: canvas.width - step, 
-                        h: canvas.height - step
-                    }),
-                    nPxCols = this._getPixelsColors(canvas, nPxs);
+                        w: canvas.width - accuracy, 
+                        h: canvas.height - accuracy
+                    }, accuracy);
+                    nPxCols = this._getPixelsColors(ctx, nPxs); // TODO: 1209ms should be optimized
 
                 // skip if px is not a boundary pixel of the feature
                 if (this._areColorsEqualToColor(P.Util.Color.areEqual, nPxCols, pxCol) === true) {
                     continue;
-                };
+                }
 
                 // is it pixel of the feature?
                 if(this._isColorInColors(P.Util.Color.areEqual, pxCol, colors)) {
@@ -38,10 +50,12 @@ var P = {
         return res;
     },
 
-    _splitRegionsByDist: function (pixels, dist) { // (Array, Number) -> Array
+    // TODO: should be optimized
+    _splitByDist: function (pixels, dist) { // (Array, Number) -> Array
         var disjointSet = new P.Struct.DisjointSet(),
             res;
 
+        //var t0 = new Date();
         for (var i = 0; i < pixels.length; i++) {
             disjointSet.add(pixels[i]);
             for (var j = 0; j < pixels.length; j++) {
@@ -49,10 +63,12 @@ var P = {
                 if (P.Util.Math.getDistance(pixels[i], pixels[j]) <= dist) {
                     if (!disjointSet.find(pixels[i], pixels[j])) {
                         disjointSet.union(pixels[i], pixels[j]);
-                    };
-                };
-            };
-        };
+                    }
+                }
+            }
+        }
+        // var t1 = new Date();
+        // console.log('_splitByDist loop: ', t1 - t0);
 
         res = disjointSet.extract();
         disjointSet.destroy();
@@ -74,14 +90,14 @@ var P = {
         return cols;
     },
 
-    _getPixelColor: function (canvas, px) { // (HTMLCanvasElement, Object) -> Array
-        return canvas.getContext('2d').getImageData(px.x, px.y, 1, 1).data;
+    _getPixelColor: function (context, px) { // (CanvasRenderingContext2D, Object) -> Array
+        return context.getImageData(px.x, px.y, 1, 1).data;
     },
 
-    _getPixelsColors: function (canvas, pxs) { // (HTMLCanvasElement, Array) -> Array
+    _getPixelsColors: function (context, pxs) { // (CanvasRenderingContext2D, Array) -> Array
         var list = [];
         for (var i = 0; i < pxs.length; i++) {
-        	list.push(canvas.getContext('2d').getImageData(pxs[i].x, pxs[i].y, 1, 1).data);
+        	list.push(context.getImageData(pxs[i].x, pxs[i].y, 1, 1).data);
         };
         return list;
     },
@@ -104,39 +120,39 @@ var P = {
         return true;
     },
 
-    _getNeighborPixels: function (px, imgSize) { // (Object, Object) -> Array
+    _getNeighborPixels: function (px, imgSize, accuracy) { // (Object, Object) -> Array
         var res = [];
 
         if (px.x > 0 && px.y > 0) {
-            res.push({ x: px.x-step, y: px.y-step }); // tl
+            res.push({ x: px.x-accuracy, y: px.y-accuracy }); // tl
         };
 
         if (px.y > 0) {
-            res.push({ x: px.x,   y: px.y-step }); // t
+            res.push({ x: px.x,   y: px.y-accuracy }); // t
         };
 
         if (px.x < imgSize.w && px.y > 0) {
-            res.push({ x: px.x+step, y: px.y-step }); // tr
+            res.push({ x: px.x+accuracy, y: px.y-accuracy }); // tr
         };
 
         if (px.x < imgSize.w) {
-            res.push({ x: px.x+step, y: px.y }); // r
+            res.push({ x: px.x+accuracy, y: px.y }); // r
         };
 
         if (px.x < imgSize.w && px.y < imgSize.h) {
-            res.push({ x: px.x+step, y: px.y+step }); // br
+            res.push({ x: px.x+accuracy, y: px.y+accuracy }); // br
         };
 
         if (px.y < imgSize.h) {
-            res.push({ x: px.x, y: px.y+step }); // b
+            res.push({ x: px.x, y: px.y+accuracy }); // b
         };
 
         if (px.x > 0 && px.y < imgSize.h) {
-            res.push({ x: px.x-step, y: px.y+step }); // bl
+            res.push({ x: px.x-accuracy, y: px.y+accuracy }); // bl
         };
 
         if (px.x > 0) {
-            res.push({ x: px.x-step, y: px.y }); // l
+            res.push({ x: px.x-accuracy, y: px.y }); // l
         };
 
         return res;
